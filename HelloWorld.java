3a50e158f19ccfc240d26f38dabb1a0e3e18618b
EC2 Default User <ec2-user@ip-172-31-7-80.ap-south-1.compute.internal>
2021-03-31 02:21:46 +0000

    dded some msg

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("happy good friday");
+    System.out.println("happy good friday");
+    System.out.println("happy good friday");
+    System.out.println("happy good friday");
+    System.out.println("happy good friday");
+    System.out.println("happy good friday");
+    System.out.println("happy good friday");
+    System.out.println("happy good friday");
+    System.out.println("happy good friday");
   }
 }
