4498873b38492fb4ab34d25aa518df1aac2b84ff
EC2 Default User <ec2-user@ip-172-31-90-188.ec2.internal>
2022-11-03 14:57:04 +0000

    added some message11

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("happy good friday!");
     System.out.println("happy good friday!");
-    System.out.println("happy good friday!");
+    System.out.println("happy good friday!!!");
    }
 }
